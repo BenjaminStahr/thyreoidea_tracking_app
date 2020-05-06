6ef3ec842db4bc7895effa41536bb6367aafe274
BenjaminStahr <BenjaminStahr97@web.de>
2020-05-06 18:16:54 +0200

    changed networkconnection to the new url

--- app/src/main/java/com/example/hashimoto_app/NetworkWorker.java
+++ app/src/main/java/com/example/hashimoto_app/NetworkWorker.java
@@ -1,29 +1,39 @@
 package com.example.hashimoto_app;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
-import com.example.hashimoto_app.backend.DataHolder;
-import com.example.hashimoto_app.backend.FileManager;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.PKIXParameters;
+import java.security.cert.TrustAnchor;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 
 public class NetworkWorker extends Worker
 {
@@ -47,12 +57,79 @@ public class NetworkWorker extends Worker
             @Override
             protected String doInBackground(Void... voids)
             {
-                //return initServerData(symptomData);
-                String query_url = "http://srvgvm33.offis.uni-oldenburg.de:8080/1/thyreodata";
+                String query_url = "https://amt-app-studien.virt.uni-oldenburg.de:8443";
                 try
                 {
+                    // Load CAs from an InputStream
+                    // (could be from a resource or ByteArrayInputStream or ...)
+                    CertificateFactory cf = CertificateFactory.getInstance("X.509");
+                    // From https://www.washington.edu/itconnect/security/ca/load-der.crt
+                    /*InputStream caInput = getResources().openRawResource(
+                            getResources().getIdentifier("FILENAME_WITHOUT_EXTENSION",
+                                    "raw", getPackageName()));
+                    //Resources.getSystem().getAssets().open("certificate.der");*/
+                    String caString = "-----BEGIN CERTIFICATE-----\n" +
+                            "MIIFgjCCBGqgAwIBAgISBGjX+Dc23NluUCbjyVh1ZiM2MA0GCSqGSIb3DQEBCwUA\n" +
+                            "MEoxCzAJBgNVBAYTAlVTMRYwFAYDVQQKEw1MZXQncyBFbmNyeXB0MSMwIQYDVQQD\n" +
+                            "ExpMZXQncyBFbmNyeXB0IEF1dGhvcml0eSBYMzAeFw0yMDA0MjAwNTIxMjBaFw0y\n" +
+                            "MDA3MTkwNTIxMjBaMDAxLjAsBgNVBAMTJWFtdC1hcHAtc3R1ZGllbi52aXJ0LnVu\n" +
+                            "aS1vbGRlbmJ1cmcuZGUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDD\n" +
+                            "F7faoFFZ3yQlhxbATfIAmmIAj1wv8tR24QJd8DpIr+6vamB8vU1DhirVcVVFajm1\n" +
+                            "zTbdgrIr3Z1nXxmsLJ8zEclyFcGx0wJZVRkyXoOjNsVsEvRPcRZJ3f7lZOT0H0PD\n" +
+                            "zxnKfCQs2ZA70WiV3PdQwiLFyAdWT0ZlWhxFU4t4xGMGYlTzGOc3WYOs7RKOJN47\n" +
+                            "MGeZCPN8vxIuN664VRK8j805Bu+qJa6t/OXyTprqzp8x3zmMLH4jHGEck6RbVErE\n" +
+                            "WVi2eqYRssp77K2qhf2lAJR2lJVHQbNW5jrSx7vgtMj2LZxdsSYSNCxwPHLbdVzi\n" +
+                            "o5GVeiyv93v6YvjO53hLAgMBAAGjggJ6MIICdjAOBgNVHQ8BAf8EBAMCBaAwHQYD\n" +
+                            "VR0lBBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMAwGA1UdEwEB/wQCMAAwHQYDVR0O\n" +
+                            "BBYEFFz78iPBxZ60S2Yja9iHhLYFipVHMB8GA1UdIwQYMBaAFKhKamMEfd265tE5\n" +
+                            "t6ZFZe/zqOyhMG8GCCsGAQUFBwEBBGMwYTAuBggrBgEFBQcwAYYiaHR0cDovL29j\n" +
+                            "c3AuaW50LXgzLmxldHNlbmNyeXB0Lm9yZzAvBggrBgEFBQcwAoYjaHR0cDovL2Nl\n" +
+                            "cnQuaW50LXgzLmxldHNlbmNyeXB0Lm9yZy8wMAYDVR0RBCkwJ4IlYW10LWFwcC1z\n" +
+                            "dHVkaWVuLnZpcnQudW5pLW9sZGVuYnVyZy5kZTBMBgNVHSAERTBDMAgGBmeBDAEC\n" +
+                            "ATA3BgsrBgEEAYLfEwEBATAoMCYGCCsGAQUFBwIBFhpodHRwOi8vY3BzLmxldHNl\n" +
+                            "bmNyeXB0Lm9yZzCCAQQGCisGAQQB1nkCBAIEgfUEgfIA8AB2AAe3XBvlfWj/8bDG\n" +
+                            "HSMVx7rmV3xXlLdq7rxhOhpp06IcAAABcZY/kzEAAAQDAEcwRQIhAOIeiiAWjmsu\n" +
+                            "yBYPjNUFP+v80t5MYpWEBboskLBNUFruAiAS+wVF9MjZ4BaS3h50q82H7CpvrIeT\n" +
+                            "5JIowpsfE2slAgB2AOcS8rA3fhpi+47JDGGE8ep7N8tWHREmW/Pg80vyQVRuAAAB\n" +
+                            "cZY/k2YAAAQDAEcwRQIgCS4gkqoBVW89eTlA/tfQATrkFQARWNVC7hYouXH5JCsC\n" +
+                            "IQC05Y1TSr91ZxNrzvoHYlvAzQ0FYLhg9ZU5XuZGSXhhpDANBgkqhkiG9w0BAQsF\n" +
+                            "AAOCAQEAdvw0gmMA3ZnoIEYEyQJdKPl2010Cegik705CZRHFEZSbIJeIgOGjA2ue\n" +
+                            "N/f3BUWPAELPEHGOIMVcRgl1i7iku2bfEJ14nqCEEPFlcXEsD2r/QkOYLDCbwYyy\n" +
+                            "iMS4SiDCIh4Ode915KpeO13l/8k2wUbzxWZ9bFxH5cRo7JcNHUylh6riUi3Pfbx3\n" +
+                            "1c9L8wkPUVMZwiFZ7MlitOy4uKuG/J6wYzCCLjAD9ZHL0UE4+hDZOo9LlrlkGU6Y\n" +
+                            "TBQ5CpwNcVpphjfCRBncik50So6IuS66GswDmDkdA2Uu/rXoadG5Jgmf4H1rakYh\n" +
+                            "mb3AyC16ykihQhWYU7DyL68M7P/IcQ==\n" +
+                            "-----END CERTIFICATE-----";
+                    InputStream caInput = new ByteArrayInputStream(caString.getBytes());
+                    Certificate ca;
+                    try
+                    {
+                        ca = cf.generateCertificate(caInput);
+                        System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+                    }
+                    finally
+                    {
+                        caInput.close();
+                    }
+
+                    // Create a KeyStore containing our trusted CAs
+                    String keyStoreType = KeyStore.getDefaultType();
+                    KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+                    keyStore.load(null, null);
+                    keyStore.setCertificateEntry("ca", ca);
+
+                    // Create a TrustManager that trusts the CAs in our KeyStore
+                    String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+                    TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+                    tmf.init(keyStore);
+
+                    // Create an SSLContext that uses our TrustManager
+                    SSLContext context = SSLContext.getInstance("TLS");
+                    context.init(null, tmf.getTrustManagers(), null);
+
                     URL url = new URL(query_url);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+                    conn.setSSLSocketFactory(context.getSocketFactory());
                     conn.setConnectTimeout(5000);
                     conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
                     conn.setDoOutput(true);
